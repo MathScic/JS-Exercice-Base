f122e386c7d5c606a585709ea37f0b96f88adea5
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-06-20 17:30:11 +0200

    ajout fonction changeColor

--- Index.js
+++ Index.js
@@ -2,29 +2,25 @@ const validation = document.querySelector("button")
 
 validation.addEventListener('click', () => {
 console.log('click');
-validation.innerHTML += '<div class="nom prenom"></div>'
+validation.innerHTML += '<div class=""></div>'
+colorChange
 }) 
 
 
-
-
-
+function colorChange() {
     const nom = document.querySelector("input[placeholder=Nom]")
     const prenom = document.querySelector("input[placeholder=Prénom]")
-    console.log(prenom);
 
     nom.addEventListener('input', () => {
         console.log('yes ');
-        nom.classList.add("nom")
+        
     })
-    
+        
     prenom.addEventListener('input', () => {
         console.log('yes..');
-        prenom.classList.add("prenom")
+        
     })
-
-
-
+}
 
 
 
