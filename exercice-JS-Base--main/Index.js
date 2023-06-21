a0d26e983cd10ee21ee1637b71454f3cf224c22f
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-06-21 10:28:52 +0200

    ajout modif vue avec mentor

---
+++ exercice-JS-Base--main/Index.js
@@ -0,0 +1,20 @@
+const validation = document.querySelector("button")
+
+validation.addEventListener('click', () => {
+console.log('click');
+validation.innerHTML += '<div class=""></div>'
+colorChange()
+}) 
+
+
+function colorChange() {
+    const nom = document.querySelector("input[placeholder=Nom]")
+    const prenom = document.querySelector("input[placeholder=Prénom]")
+
+}
+
+
+
+
+
+
