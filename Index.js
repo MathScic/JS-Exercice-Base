8ced51d9bb87e1a70ee8d0093f9bd36f08255959
Math Scicl <scicluna.mathieu@hotmail.fr>
2023-06-22 10:08:10 +0200

    ajout const nom et prenom dans event listener

--- Index.js
+++ Index.js
@@ -2,8 +2,11 @@ const validation = document.querySelector("btn-1");
 
 validation.addEventListener("click", () => {
   console.log("click");
-  colorChange();
-});
+  const name = document.getElementById("#nom").value;
+  const firstName = document.getElementById("#prenom").value;
+})
+
+validation.document.addEventListener
 
 function colorChange() {
   const nom = document.querySelector("input[placeholder=Nom]");
